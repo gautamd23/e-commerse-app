1cea77dd935360e3af00b68a42488253f8c0aa24
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-02-20 21:48:35 +0530

    created routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './index.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Shop from './Pages/Shop';
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path:'/',
+      element:<Shop/>
+    }
+  ])
   return (
     <div className=''>
       <Navbar/>
+      <RouterProvider router={appRouter}/>
     </div>
   );
 }
