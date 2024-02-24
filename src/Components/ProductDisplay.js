b1dd65ecef8a476b3b51d1a4ce429e11a4b07cf3
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-02-24 14:53:21 +0530

    fix image display

--- src/Components/ProductDisplay.js
+++ src/Components/ProductDisplay.js
@@ -3,16 +3,17 @@ import React from "react";
 export default function ProductDisplay({ product }) {
   return (
     <div className="px-10 flex justify-center gap-7">
-      <div className="grid grid-flow-col w-[600px]">
-        <div className="">
-          <img className="w-[100px]" src={product.image}></img>
-          <img className="w-[100px]" src={product.image}></img>
-          <img className="w-[100px]" src={product.image}></img>
-          <img className="w-[100px]" src={product.image}></img>
-          <img className="w-[100px]" src={product.image}></img>
+      <div className="grid  grid-flow-col  w-[600px] ">
+        <div className="grid grid-row-5 grid-cols-1 gap-2 h-full">
+          <img className="w-[100px] h-[110px]" src={product.image}></img>
+          <img className="w-[100px] h-[110px]" src={product.image}></img>
+          <img className="w-[100px] h-[110px]" src={product.image}></img>
+          <img className="w-[100px] h-[110px]" src={product.image}></img>
+          <img className="w-[100px] h-[110px]" src={product.image}></img>
+          
         </div>
-        <div className="">
-          <img className="" src={product.image}></img>
+        <div className="grid-cols-1 row-span-5 ml-1">
+          <img className="row-span-5 h-full" src={product.image}></img>
         </div>
       </div>
       <div>
